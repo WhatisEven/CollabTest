5ee5c02a65357e02bb9424dc76fd00238eca3701
WhatisEven <164270232+WhatisEven@users.noreply.github.com>
2024-12-16 14:09:54 -0500

    Add files via upload

---
+++ Fish.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.Random;
+/**
+ * Write a description of class Fish here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fish extends Actor
+{
+    /**
+     * Act - do whatever the Fish wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    FishControl fishControl = new FishControl();
+    public int currentSpeed = 0;
+    
+    public void act()
+    {
+        // Add your action code here.
+        fishControl.startAppearing(this);
+        spawnLocation();
+        fishControl.endFading(this);
+        ((MyWorld)getWorld()).reachEnd(this);
+    }
+    
+    private void spawnLocation()
+    {
+            setLocation(currentSpeed += fishControl.speed, fishControl.yLocation);
+    }
+}
