f1f8b867c870f51537ff1eea5ef567c121cfed32
WhatisEven <164270232+WhatisEven@users.noreply.github.com>
2025-01-16 14:49:45 -0500

    Add files via upload

---
+++ WorldSurvival-main/PlayerEnergyBar.java
@@ -0,0 +1,52 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PlayerHealthBAr here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PlayerEnergyBar extends PlayerStatusBar
+{
+    /**
+     * Act - do whatever the PlayerHealthBAr wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */    
+    
+    public PlayerEnergyBar()
+    {
+        statusText = new ScreenLabel(Player.getEnergyPoint(), 50);
+    }
+    
+    public void act()
+    {
+        super.act();
+    }
+    
+    /**
+     * A method that displays the current health of the player
+     */
+    public void displayText()
+    {
+        statusText.setValue(Player.getEnergyPoint());
+    }
+    
+    /**
+     * A method that updates the filling of the health bar base on the remaining health of the player
+     */
+    public void updateBar()
+    {
+        borderImage = new GreenfootImage(240, 50); // Creates a brown border for the health bar
+        remainImage = new GreenfootImage(200, 36); // Creates a flexible green color block to represent health remaining for the player
+        backgroundImage = new GreenfootImage(200, 36); // Creates a red color block to represent the background for the health lost  
+        borderImage.setColor(new Color(101, 67, 33));
+        remainImage.setColor(new Color (154, 254, 255));
+        backgroundImage.setColor(new Color (78, 226, 236));
+        borderImage.fillRect(0, 0, 214, 50);
+        remainImage.fillRect(0, 0, (int)(((double)Player.getEnergyPoint() / Player.getMaxEnergy()) * 200), 36);
+        backgroundImage.fillRect(0, 0, 200, 36);
+        borderImage.drawImage(backgroundImage, 7, 7); // Draws the red block onto the border
+        borderImage.drawImage(remainImage, 7, 7); // Draws the green block onto the border to overlap the red block
+        setImage(borderImage);
+    }
+}
---
+++ WorldSurvival-main/WorkShop.java
@@ -0,0 +1,52 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WorkShop here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class WorkShop extends Actor
+{
+    private boolean isOpen;
+    protected static final int HOLDER_SIZE = 220;
+    /**
+     * A getter method that gets the current opening state of the workshop
+     */
+    public boolean isOpen()
+    {
+        return isOpen;
+    }
+
+    /**
+     * A method that indicates opening of the workshop
+     */
+    public void openWorkShop()
+    {
+        isOpen = true;
+        addHolders(); // Add all the work shop holders to the world
+    }
+
+    /**
+     * A method that indicates closing of the workshop
+     */
+    public void closeWorkShop()
+    {
+        isOpen = false;
+        getWorld().removeObjects(getWorld().getObjects(WorkShopHolder.class)); // Remove all the work shop holders in the world
+    }
+
+    /**
+     * A method that displays all the shop items 
+     */
+    private void addHolders()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int f = 0; f < 5; f++)
+            {
+                getWorld().addObject(new WorkShopHolder(), getX() - getImage().getWidth() / 2 + 140 + f * 280, getY() - getImage().getHeight() / 2 + 280 + i * 280);
+            }
+        }
+    }
+}
